7f3a327e7b0c93bf2410a871459b74ef73a9a5bf
johnli56 <li.johnny34@gmail.com>
2018-05-21 15:55:58 -0700

    Created SkillPlan activity for skill plan button
    .

---
+++ skilleden/app/src/main/java/com/eve/skilleden/SkillPlanActivity.java
@@ -0,0 +1,13 @@
+package com.eve.skilleden;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SkillPlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_skill_plan);
+    }
+}
