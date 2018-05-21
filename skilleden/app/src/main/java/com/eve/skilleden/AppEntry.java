7822c99da5ed052eccf602bb736c86c7e892940f
johnli56 <li.johnny34@gmail.com>
2018-05-21 15:52:54 -0700

    Cleaned files
    .

--- skilleden/app/src/main/java/com/eve/skilleden/AppEntry.java
+++
@@ -1,6 +0,0 @@
-package com.eve.skilleden;
-
-import android.app.Application;
-
-public class AppEntry extends Application {
-}
--- skilleden/app/src/main/java/com/eve/skilleden/viewmodel/SkillGroupViewModel.java
+++
@@ -1,6 +0,0 @@
-package com.eve.skilleden.viewmodel;
-
-public class SkillGroupViewModel {
-
-    //    public ObservableField<SkillList> skillListObservableField = new ObservableField<>();
-}
